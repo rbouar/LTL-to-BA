c0cf1b55a6df17075fbf15a6f2ee2147466d481b
BOUARAH Romain <romain.bouarah@etu.univ-paris-diderot.fr>
2021-04-21 17:47:21 +0200

    Add function for retrieving variables

--- prototype/ltl.ml
+++ prototype/ltl.ml
@@ -1,3 +1,5 @@
+module SetString = Set.Make(String);;
+
 type ltlFormula =
   | Const of bool
   | Var of string
@@ -15,3 +17,10 @@ let rec ltlFormula_to_string = function
   | Not f -> "(NOT " ^ ltlFormula_to_string f ^ ")"
   | Next f -> "(NEXT " ^ ltlFormula_to_string f ^ ")"
   | Until (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " UNTIL " ^ ltlFormula_to_string f2 ^ ")"
+
+
+let rec get_variables = function
+  | Const _ -> SetString.empty
+  | Var v -> SetString.singleton v
+  | Or (f1,f2) | And (f1,f2) | Until (f1,f2) -> SetString.union (get_variables f1) (get_variables f2)
+  | Next f | Not f -> get_variables f
