ab69be3f9b7d5b87cdb2c1ae008aa59ea1d2d2e6
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2021-06-06 21:12:29 +0200

    Add colors to states on .dot files

--- prototype/export.ml
+++ prototype/export.ml
@@ -7,6 +7,34 @@ let setstring_to_string set =
 let remove_if_exist filename =
   if Sys.file_exists filename then Sys.remove filename
 
+let split_states buchi =
+  let state_equal = List.for_all2 equals in
+  let is_in_states s = List.exists (state_equal s) in
+  let rec fill_all states basic inits finals both = match states with
+    | [] -> basic, inits, finals, both
+    | s :: states ->
+      if is_in_states s buchi.init_states then
+        if is_in_states s buchi.final_states then
+          fill_all states basic inits finals (s :: both)
+        else fill_all states basic (s :: inits) finals both
+      else if is_in_states s buchi.final_states then
+        fill_all states basic inits (s :: finals) both
+      else fill_all states (s :: basic) inits finals both
+  in fill_all buchi.states [] [] [] []
+
+let states_to_export_string buchi =
+  let rec export_states_aux states color = match states with
+    | [] -> ""
+    | s :: states -> "\"" ^ state_to_string s ^ "\"" ^ "[color=" ^ color ^ "]\n" ^
+    export_states_aux states color  in
+  let basic, inits, finals, both = split_states buchi in
+  export_states_aux basic "white" ^
+  export_states_aux inits "cyan" ^
+  export_states_aux finals "yellow" ^
+  export_states_aux both "green"
+
+
+
 let one_transition_to_string from_state to_state alpha_set =
   "\""
   ^ state_to_string from_state
@@ -25,7 +53,7 @@ let buchi_to_dot buchi filename =
   let _ = remove_if_exist filename in
   let c_out = open_out filename in
   let _ = output_string c_out "digraph buchi_automaton {\n" in
+  let _ = output_string c_out (states_to_export_string buchi) in
   let _ = export_transitions buchi.eval c_out in
   let _ = output_string c_out "}\n" in
   close_out c_out
-
--- prototype/ltl.ml
+++ prototype/ltl.ml
@@ -9,22 +9,6 @@ type ltlFormula =
   | Next of ltlFormula
   | Until of ltlFormula * ltlFormula
 
-let rec ltlFormula_to_string = function
-  | Const b -> string_of_bool b
-  | Var v -> v
-  | Or (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " OR " ^ ltlFormula_to_string f2 ^ ")"
-  | And (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " AND " ^ ltlFormula_to_string f2 ^ ")"
-  | Not f -> "(NOT " ^ ltlFormula_to_string f ^ ")"
-  | Next f -> "(NEXT " ^ ltlFormula_to_string f ^ ")"
-  | Until (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " UNTIL " ^ ltlFormula_to_string f2 ^ ")"
-
-
-let rec get_variables = function
-  | Const _ -> SetString.empty
-  | Var v -> SetString.singleton v
-  | Or (f1,f2) | And (f1,f2) | Until (f1,f2) -> SetString.union (get_variables f1) (get_variables f2)
-  | Next f | Not f -> get_variables f
-
 let rec equals ltlf1 ltlf2 = match ltlf1, ltlf2 with
   | Const b1, Const b2 -> b1 = b2
   | Var s1, Var s2 -> s1 = s2
@@ -36,6 +20,27 @@ let rec equals ltlf1 ltlf2 = match ltlf1, ltlf2 with
   | Next ne1, Next ne2 -> equals ne1 ne2
   | Until (ul1, ur1), Until (ul2, ur2) -> (equals ul1 ul2) && (equals ur1 ur2)
   | _, _ -> false
+
+
+let rec ltlFormula_to_string = function
+  | Const b -> string_of_bool b
+  | Var v -> v
+  | Or (Not(f1), f2) -> "("^ ltlFormula_to_string f1 ^" => "^ ltlFormula_to_string f2 ^ ")"
+  | Or (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " || " ^ ltlFormula_to_string f2 ^ ")"
+  | And (Or(Not(f1), f2), Or(Not(f4), f3)) when (equals f1 f3) && (equals f2 f4)  -> "(" ^ ltlFormula_to_string f1^ " <=> " ^ ltlFormula_to_string f2 ^ ")"
+  | And (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " && " ^ ltlFormula_to_string f2 ^ ")"
+  | Not (Until (Const true, Not(f))) -> "(G "^ ltlFormula_to_string f^")"
+  | Not f -> "(! " ^ ltlFormula_to_string f ^ ")"
+  | Next f -> "(X " ^ ltlFormula_to_string f ^ ")"
+  | Until (Const true, f) -> "(F "^ ltlFormula_to_string f ^ ")"
+  | Until (f1, f2) -> "(" ^ ltlFormula_to_string f1 ^ " U " ^ ltlFormula_to_string f2 ^ ")"
+
+
+let rec get_variables = function
+  | Const _ -> SetString.empty
+  | Var v -> SetString.singleton v
+  | Or (f1,f2) | And (f1,f2) | Until (f1,f2) -> SetString.union (get_variables f1) (get_variables f2)
+  | Next f | Not f -> get_variables f
 
 let get_list_of_untils f =
   let rec get_list_of_untils_aux f acc = match f with
