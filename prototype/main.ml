9daf0e80886e5a51dfde74635527d9428476beea
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2021-06-07 21:52:19 +0200

    Add number of states and number of transitions in main.ml

--- prototype/main.ml
+++ prototype/main.ml
@@ -5,4 +5,8 @@ let _ =
   let lexbuf = Lexing.from_channel stdin in
   let formula = Parser.start Lexer.main lexbuf in
   let buchi_automaton = Buchi.create_automaton formula in
+  let _ = Printf.printf "Nombre d'états: %d\n"
+      (List.length buchi_automaton.states) in
+  let _ = Printf.printf "Nombre de transitions: %d\n"
+      (Hashtbl.fold (fun _ y c -> c + (Hashtbl.length y)) buchi_automaton.eval 0) in
   Export.buchi_to_dot buchi_automaton "buchi.dot"
